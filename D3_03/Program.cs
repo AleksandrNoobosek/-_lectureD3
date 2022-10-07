0c9bb4380a1546d2dfdf43702cd36efc9cc91d0c
Aleksandr <Aleksander.n.gusev@mail.ru>
2022-10-07 17:17:31 +0300

    add: D3_03

---
+++ D3_03/Program.cs
@@ -0,0 +1,28 @@
+﻿//Найти максимальное из трех чисел
+
+
+Console.Write("Введите число А: ");
+int a = int.Parse(Console.ReadLine());  //int a = int.Parse(Console.Readine ());
+
+
+Console.Write("введите число B: ");
+int b = int.Parse(Console.ReadLine());
+
+
+Console.Write("введите число C: ");
+int c = int.Parse(Console.ReadLine());
+
+
+int max = a;
+
+if (b>max) 
+{
+	max = b;
+}
+else if (c>max) 
+{
+	max = c;
+}
+
+Console.Write("Максимальное значене: ");
+Console.Write(max);
