37fed7b36c51b1242b190bc83b94f7d2157ac1b5
Aleksandr <Aleksander.n.gusev@mail.ru>
2022-10-07 18:35:23 +0300

    feat: добавил вариант решения в //

--- D3_11/Program.cs
+++ D3_11/Program.cs
@@ -18,3 +18,17 @@ else
 	Console.WriteLine("третей цифры числа нет!");
 }
 
+//int num = new Random().Next(1,10000);
+//Console.Write("Рандомное значение: ");
+//Console.WriteLine(num);
+//
+//if(num > 99)
+//{
+//    Console.Write("Третья цифра числа справа на лево ");
+//    int result= (num / 100) % 10;
+//    Console.WriteLine(result);
+//}
+//else
+//{
+//   Console.WriteLine("третей цифры числа нет!");
+//}
