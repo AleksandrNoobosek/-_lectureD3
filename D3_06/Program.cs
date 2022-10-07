358c46cf638d0f1cc8d3f21ece95b0b05673bdce
Aleksandr <Aleksander.n.gusev@mail.ru>
2022-10-07 17:18:10 +0300

    add: D3_06

---
+++ D3_06/Program.cs
@@ -0,0 +1,8 @@
+﻿// Показать последнюю цифру трёхзначного числа
+int num = new Random().Next(100,1000);
+Console.Write("Рандомное значение: ");
+Console.WriteLine(num);
+
+int LastNum = num%10;
+Console.Write("последняя цифра трёхзначного числа: ");
+Console.WriteLine(LastNum);
