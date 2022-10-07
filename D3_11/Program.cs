a8fadfe584ee87eba652bc2d086929b2b7ccaa40
Aleksandr <Aleksander.n.gusev@mail.ru>
2022-10-07 17:19:26 +0300

    add: D3_11

---
+++ D3_11/Program.cs
@@ -0,0 +1,20 @@
+﻿//Найти третью цифру числа или сообщить, что её нет
+
+Console.WriteLine("Введите число: ");
+double num = double.Parse(Console.ReadLine());
+if(num > 99)
+{
+    string Array = num.ToString();
+    int size = Array.Length;
+    int i = size-3;
+    Console.Write("Третья цифра числа слева на право >>");
+	Console.WriteLine(Array[2]);
+    Console.Write("Третья цифра числа справа на лево ");
+    Console.Write(Array[i]);
+    Console.WriteLine("<<");
+}
+else
+{
+	Console.WriteLine("третей цифры числа нет!");
+}
+
