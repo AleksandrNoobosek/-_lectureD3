610a7ca48407f0396b96235ab129c93b03f57629
Aleksandr <Aleksander.n.gusev@mail.ru>
2022-10-07 17:15:53 +0300

    add: D3_01

---
+++ D3_01/Program.cs
@@ -0,0 +1,18 @@
+﻿//По двум заданным числам проверять является ли первое квадратом второго
+
+Console.Write("введите число: ");
+int num1 = int.Parse(Console.ReadLine() ?? "0");
+
+Console.Write("введите число: ");
+int num2 = int.Parse(Console.ReadLine()?? "0");
+
+int num = num1*num1;
+
+if(num==num2)
+{
+    Console.WriteLine("первое яв-ся квадратом второго! ");
+}
+else
+{
+    Console.WriteLine("первое не яв-ся квадратом второго! ");
+}
