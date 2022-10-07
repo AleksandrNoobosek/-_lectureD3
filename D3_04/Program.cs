7ed8a266c009a1197fd809cd1efa0280dcb93584
Aleksandr <Aleksander.n.gusev@mail.ru>
2022-10-07 17:17:44 +0300

    add: D3_04

---
+++ D3_04/Program.cs
@@ -0,0 +1,14 @@
+﻿//Выяснить является ли число чётным
+
+Console.Write("введите число: ");
+int num = int.Parse(Console.ReadLine());
+
+
+if(num%2==0)
+{
+    Console.WriteLine("Является четным ");
+}
+else
+{
+    Console.WriteLine("не является четным ");
+}
