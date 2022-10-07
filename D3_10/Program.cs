9021b07197cbe98917ba4b3948922a600041baba
Aleksandr <Aleksander.n.gusev@mail.ru>
2022-10-07 17:19:08 +0300

    add: D3_10

---
+++ D3_10/Program.cs
@@ -0,0 +1,33 @@
+﻿// Выяснить, кратно ли число заданному, если нет, вывести остаток.
+
+int numRan = new Random().Next(10,50);
+Console.Write("Рандомное значение: ");
+Console.WriteLine(numRan);
+
+Console.Write("введите число: ");
+int numTer = int.Parse(Console.ReadLine());
+Console.Write("Введенное число: ");
+Console.WriteLine(numTer);
+
+
+if (numRan%numTer==0)
+{
+    Console.Write("введенное число ");
+    Console.Write(numTer);
+    Console.Write(" ,кратно рандомно заданному числу ");
+    Console.Write(numRan);
+}
+else if (numTer%numRan==0)
+{
+    Console.Write("Рандомное число ");
+    Console.Write(numRan);
+    Console.Write(" ,кратно введенному числу ");
+    Console.Write(numTer);
+}
+else 
+{
+    Console.Write("введенное число не кратно рандомно заданному числу. ");
+    double num1 = numRan % numTer;
+    Console.Write("Остаток числа: ");
+    Console.Write(num1);
+}
