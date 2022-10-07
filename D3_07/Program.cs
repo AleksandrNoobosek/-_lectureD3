d668de040f6501dbcacacd82a065bcfb904a19a5
Aleksandr <Aleksander.n.gusev@mail.ru>
2022-10-07 17:18:21 +0300

    add: D3_07

---
+++ D3_07/Program.cs
@@ -0,0 +1,9 @@
+﻿// Показать вторую цифру трёхзначного числа
+
+int num = new Random().Next(100,1000);
+Console.Write("Рандомное значение: ");
+Console.WriteLine(num);
+
+int LastNum = ((num/10)%10);
+Console.Write("вторуя цифра трёхзначного числа: ");
+Console.WriteLine(LastNum);
