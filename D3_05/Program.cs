715915bdf5573962cebfed16483f2099919cf684
Aleksandr <Aleksander.n.gusev@mail.ru>
2022-10-07 17:17:59 +0300

    add: D3_05

---
+++ D3_05/Program.cs
@@ -0,0 +1,25 @@
+﻿
+// Показать четные числа от 1 до N
+
+Console.Write("введите число N: ");
+int N = int.Parse(Console.ReadLine());
+
+
+int a = 1;
+
+while (a <= N)
+{
+	if(a%2==0)
+	{
+        Console.Write("четное число: ");
+		Console.WriteLine(a);
+	}
+	
+	else
+	{
+		Console.WriteLine("-");
+		
+	}
+
+	a=a+1;
+}	
