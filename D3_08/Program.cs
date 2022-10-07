d1d7a6c0a0d017b7fe379f55b8c3d5913f9a8095
Aleksandr <Aleksander.n.gusev@mail.ru>
2022-10-07 17:18:30 +0300

    add: D3_08

---
+++ D3_08/Program.cs
@@ -0,0 +1,28 @@
+﻿// Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
+
+int num = new Random().Next(10,100);
+Console.Write("Рандомное значение: ");
+Console.WriteLine(num);
+
+int num1 = num/10;
+int num2 = num%10;
+
+if(num1>num2)
+{
+    Console.Write("наибольшая цифра числа: ");
+    Console.WriteLine(num1);
+}
+else if (num2>num1) 
+{
+    Console.Write("наибольшая цифра числа: ");
+    Console.WriteLine(num2);
+}
+
+else if (num1==num2)
+{
+    Console.Write("они равны: ");
+    Console.Write(num1);
+    Console.Write("=");
+    Console.Write(num2);
+
+}
